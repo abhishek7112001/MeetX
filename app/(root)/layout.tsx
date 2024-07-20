654849e85b22c64726c3b3719fed23eb10ab1a42
Abhishek Kumar <abhisheklenovo2001@gmail.com>
2024-07-20 15:48:11 +0530

    initialisation done

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,22 @@
 import { ReactNode } from 'react';
 
 import StreamVideoProvider from '@/providers/StreamClientProvider';
+import Sidebar from '@/components/Sidebar';
 
 const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
   return (
-    <main>
-      <StreamVideoProvider>{children}</StreamVideoProvider>
+    <main className='relative'>
+      {/* <Navbar /> */}
+      <div className='flex'>
+        <Sidebar />
+
+        <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14'>
+          <div className='w-full'>
+            {children}
+          </div>
+
+        </section>
+      </div>
     </main>
   );
 };
