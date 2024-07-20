89b582f96e67c6c4d492b0129b7bbc65c0d84eb9
Abhishek Kumar <abhisheklenovo2001@gmail.com>
2024-07-20 17:32:00 +0530

    stream api added and project completed

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -4,12 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetTrigger,
-} from '@/components/ui/sheet';
+import { Sheet, SheetClose, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import { sidebarLinks } from '@/constants';
 import { cn } from '@/lib/utils';
 
@@ -53,7 +48,7 @@ const MobileNav = () => {
                           'flex gap-4 items-center p-4 rounded-lg w-full max-w-60',
                           {
                             'bg-blue-1': isActive,
-                          },
+                          }
                         )}
                       >
                         <Image
